dc187cdfa57a94bb7bcbb0cf89f95727cb374dc7
david-dom <113909984+david-dom@users.noreply.github.com>
2023-09-27 17:42:28 -0500

    My commit message

--- src/app/screen/page.tsx
+++ src/app/screen/page.tsx
@@ -22,9 +22,9 @@ function Screen() {
         return(
             <div className={"baseScreen hide-native-scrollbar " + styles["Screen"]}>
                 
-<div id="layer-1-31e565f909cd4dc9903f58577fa63399" className={"baseLayer hide-native-scrollbar " + styles["layer-1-31e565f909cd4dc9903f58577fa63399"]}    >
+<div id="header-31e565f909cd4dc9903f58577fa63399" className={"baseLayer hide-native-scrollbar " + styles["header-31e565f909cd4dc9903f58577fa63399"]}    >
 <div className={"baseImageView " + styles["imageview-1-0725be7b5a5d461c9c94c10ac760fca4"]}  >
-    <Image id="imageview-1-0725be7b5a5d461c9c94c10ac760fca4" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-1" src='/assets/Wordpress-Logo-Black.png' />
+    <Image id="imageview-1-0725be7b5a5d461c9c94c10ac760fca4" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-1" src='/assets/Wordpress-Logo-White.png' />
 </div>
 <Link href="/screen"><div id="text-1-3f8b56f7b2b04630baf5df3e3e31e2f1" className={" baseText " + styles["text-1-3f8b56f7b2b04630baf5df3e3e31e2f1"]}   >
     Productos
@@ -45,7 +45,7 @@ function Screen() {
     Comenzar
 </div>
 </div>
-<div id="layer-3-512c753b18f740df8cf191cc91b80d3c" className={"baseLayer hide-native-scrollbar " + styles["layer-3-512c753b18f740df8cf191cc91b80d3c"]}    >
+<div id="seccion_1-512c753b18f740df8cf191cc91b80d3c" className={"baseLayer hide-native-scrollbar " + styles["seccion_1-512c753b18f740df8cf191cc91b80d3c"]}    >
 <div id="text-11-77739d2a5050484ab3971d8853bf073f" className={" baseText " + styles["text-11-77739d2a5050484ab3971d8853bf073f"]}   >
     Bienvenido al creador de webs
 más querido del mundo
@@ -55,8 +55,49 @@ más querido del mundo
 
 Empieza ahora
 </div>
+<div id="layer-6-5c16f82c5c054dc0ba175a10c295a11f" className={"baseLayer hide-native-scrollbar " + styles["layer-6-5c16f82c5c054dc0ba175a10c295a11f"]}    >
 <div id="text-13-8c01d4e08ddf4eb6a8110ac02cdf9d5c" className={" baseText " + styles["text-13-8c01d4e08ddf4eb6a8110ac02cdf9d5c"]}   >
     Empieza Ahora
+</div>
+</div>
+</div>
+<div id="seccion_2-d96ff02f69bd48aa82b1809fe899872b" className={"baseLayer hide-native-scrollbar " + styles["seccion_2-d96ff02f69bd48aa82b1809fe899872b"]}    >
+<div id="layer-8-f0d12d9477724fdea629aa0d621406f6" className={"baseLayer hide-native-scrollbar " + styles["layer-8-f0d12d9477724fdea629aa0d621406f6"]}    >
+<div className={"baseImageView " + styles["imageview-4-d2a33be1db344b08842dc78484606ffb"]}  >
+    <Image id="imageview-4-d2a33be1db344b08842dc78484606ffb" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-4" src='/assets/theme-2-2x-2.png' />
+</div>
+<div className={"baseImageView " + styles["imageview-5-61c5bda840734d0fb4e8175d89d2fe5c"]}  >
+    <Image id="imageview-5-61c5bda840734d0fb4e8175d89d2fe5c" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-5" src='/assets/theme-3-2-2x.png' />
+</div>
+<div className={"baseImageView " + styles["imageview-6-b3f1c62850aa44988a1f46d70f94d3b6"]}  >
+    <Image id="imageview-6-b3f1c62850aa44988a1f46d70f94d3b6" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-6" src='/assets/theme-3-2x-3.png' />
+</div>
+</div>
+</div>
+<div id="-8e6fdd0e16564b298fd905fefdfc6b55" className={"baseLayer hide-native-scrollbar " + styles["-8e6fdd0e16564b298fd905fefdfc6b55"]}    >
+<div id="layer-9-ef37bf3043004fa4be58d9bae2dc3f89" className={"baseLayer hide-native-scrollbar " + styles["layer-9-ef37bf3043004fa4be58d9bae2dc3f89"]}    >
+<div className={"baseImageView " + styles["imageview-7-0797203686b1459b8b630fc2fb7cf8f1"]}  >
+    <Image id="imageview-7-0797203686b1459b8b630fc2fb7cf8f1" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-7" src='/assets/theme-6-2x-2.png' />
+</div>
+<div className={"baseImageView " + styles["imageview-8-f75d47ffc44042c8ac6ad427ad1782c3"]}  >
+    <Image id="imageview-8-f75d47ffc44042c8ac6ad427ad1782c3" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-8" src='/assets/theme-7-2x-2.png' />
+</div>
+<div className={"baseImageView " + styles["imageview-9-02489036ef6b407ab8dc0cef48aff944"]}  >
+    <Image id="imageview-9-02489036ef6b407ab8dc0cef48aff944" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-9" src='/assets/theme-8-2x-2.png' />
+</div>
+</div>
+</div>
+<div id="seccion2.2-c75e0ce2713e461a8e086c9d0ea0e9e6" className={"baseLayer hide-native-scrollbar " + styles["seccion2.2-c75e0ce2713e461a8e086c9d0ea0e9e6"]}    >
+<div id="layer-12-7b9a0378ebaa49329ef739f059628b9a" className={"baseLayer hide-native-scrollbar " + styles["layer-12-7b9a0378ebaa49329ef739f059628b9a"]}    >
+<div className={"baseImageView " + styles["imageview-10-0f5780a8726e4612947ebd5a33230b84"]}  >
+    <Image id="imageview-10-0f5780a8726e4612947ebd5a33230b84" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-10" src='/assets/theme-9-2x-2.png' />
+</div>
+<div className={"baseImageView " + styles["imageview-11-cf68ce8347c64a0dbb091e80d93ab884"]}  >
+    <Image id="imageview-11-cf68ce8347c64a0dbb091e80d93ab884" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-11" src='/assets/theme-11-2x.png' />
+</div>
+<div className={"baseImageView " + styles["imageview-12-ca511b6224cb449996cc2009eaa0a46f"]}  >
+    <Image id="imageview-12-ca511b6224cb449996cc2009eaa0a46f" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{ objectFit: 'contain' }} fill={true} alt="imageview-12" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+</div>
 </div>
 </div>
             </div>
